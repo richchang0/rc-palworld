9aca2b7f278a2119130ed416d0975a2ff4c1a92b
rich <richchang0@gmail.com>
2024-02-09 23:41:13 +0000

    backup via cmd

--- cmd/stop.sh
+++ cmd/stop.sh
@@ -1,3 +1,8 @@
 cd /home/steam/Steam/steamapps/common/PalServer/Pal/Saved/
 date >> log.txt
 echo "stop cmd starting" >> log.txt
+echo "Save" | rcon -s localhost >> log.txt &
+sleep 10
+echo "DoExit" | rcon -s localhost >> log.txt
+/home/steam/Steam/steamapps/common/PalServer/Pal/Saved/cmd/backup.sh
+
